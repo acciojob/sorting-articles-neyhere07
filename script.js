e54491afec089568d1eaefdd07cdd75e087f4d76
Neyna Nayak <140084295+neyhere07@users.noreply.github.com>
2024-07-16 11:37:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,26 @@
 //your JS code here. If required.
+
 const bands = [
-    'The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 
-    'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 
-    'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 
-    'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'
+    'The Plot in You', 
+    'The Devil Wears Prada', 
+    'Pierce the Veil', 
+    'Norma Jean', 
+    'The Bled', 
+    'Say Anything', 
+    'The Midway State', 
+    'We Came as Romans', 
+    'Counterparts', 
+    'Oh, Sleeper', 
+    'A Skylit Drive', 
+    'Anywhere But Here', 
+    'An Old Dog'
 ];
-
-function strip(article) {
-    const wordsToIgnore = ['a', 'an', 'the'];
-    const words = article.split(' ');
-
-    if (wordsToIgnore.includes(words[0].toLowerCase())) {
-        return words.slice(1).join(' ');
-    }
-    return article;
+function replaced(article) {
+    return article.replace(/^(a |an |the )/i,'').trim();
 }
-
-const sortedBands = bands.sort((a, b) => strip(a).localeCompare(strip(b)));
-
-const ul = document.getElementById('band');
-
-sortedBands.forEach(band => {
-    const li = document.createElement('li');
-    li.textContent = band;
-    ul.appendChild(li);
+const sortedBands = bands.sort((a, b) => replaced(a).localeCompare(replaced(b)));
+const ul = document.getElementById('bands');
+sortedBands.forEach(sortBand => {
+    const li=`<li>${sortBand}</li>`;
+	ul.innerHTML+=li;
 });
